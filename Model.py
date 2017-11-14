4e670d4526038b83f81e14fbb7ce530c3069d943
rottren <nicholasgatehouse@gmail.com>
2017-11-13 17:22:20 -0800

    Created outline for the model

--- Model.py
+++ Model.py
@@ -1,4 +1,20 @@
 import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from numpy import np
 
 # length of outputs = 31
 # length of inputs = 34
+
+
+def main():
+
+    np.set_printoptions(threshold=100000)
+    model = Sequential()
+    model.add(Dense(18, input_dim=34, activation='relu'))
+    model.add(Dense(18, activation='relu'))
+    model.add(Dense(31, activation = 'sigmoid'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model.compile(loss='mse', optimizer='adam')
+    return model
